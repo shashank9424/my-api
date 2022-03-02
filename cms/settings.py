4fe1799836b69fe38ab66a203a8b694b9dffb5bd
shashank <shashankpathe@gmail.com>
2022-03-02 15:42:31 +0530

    new

--- cms/settings.py
+++ cms/settings.py
@@ -44,10 +44,9 @@ MIDDLEWARE = [
     
 ]
 CORS_ALLOWED_ORIGINS = [
-    "https://example.com",
-    "https://sub.example.com",
-    "http://localhost:3000",
-    "http://127.0.0.1:9000",
+    "https://shashank-api.herokuapp.com/",
+    "https://shashank-api.herokuapp.com/",
+   
 ]
 
 
