cc6b634216f699cce5de900f0209ee4125b8de65
shashank <shashankpathe@gmail.com>
2022-03-02 16:11:41 +0530

    new

--- cms/settings.py
+++ cms/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
 'default': {
     'ENGINE': 'django.db.backends.mysql',
     'NAME': 'task2',
-    'HOST': '127.0.0.1',
+    'HOST': 'shashank-api.herokuapp.com',
     'PORT': '3306',
     'USER': 'root',
     'PASSWORD': 'root',
